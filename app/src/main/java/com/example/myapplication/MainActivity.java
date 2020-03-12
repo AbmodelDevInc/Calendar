034f689f2b7fb9542c69cdc3dfe4536fffe690a4
caldera <agustincalderon058@gmail.com>
2020-03-12 12:13:27 -0300

    Nuevo boton para fecha

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,25 +2,44 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     EditText Recordatorio;
     EditText day;
     EditText month;
     EditText year;
     EditText Time;
 
+    Button bFecha;
+
+
+    private int dia;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Recordatorio = findViewById(R.id.Recordatorio);
+        bFecha = (Button)findViewById(R.id.bFecha);
+       // cdia = (EditText)findViewById(R.id.cdia);
         day = findViewById(R.id.day);
+
+        bFecha.setOnClickListener(this);
+
+
+        Recordatorio = findViewById(R.id.Recordatorio);
+       // day = findViewById(R.id.day);
         month = findViewById(R.id.month);
         year = findViewById(R.id.year);
         Time = findViewById(R.id.time);
@@ -46,6 +65,23 @@ public class MainActivity extends AppCompatActivity {
             else{
                 Toast.makeText(this, "se guardo el recordatorio" + r + "Para el:" + dd + "/" + mm + "/" + yy, Toast.LENGTH_SHORT).show();
             }
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v== bFecha){
+            final Calendar c = Calendar.getInstance();
+            dia = c.get(Calendar.DAY_OF_MONTH);
+            DatePickerDialog datePickerDialog;
+            datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                    day.setText(dayOfMonth+"");
+                }
+            }
+            ,dia);
+            datePickerDialog.show();
         }
     }
 }
